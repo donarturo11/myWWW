8911bdf541bb6ec9e7ad578d771b0215cd83a83d
donarturo <arturwrona91@gmail.com>
2022-06-10 13:58:24 +0200

    Improve email messages

--- myWWW/contactform.php
+++ myWWW/contactform.php
@@ -6,31 +6,48 @@ $lang = "en";
 }
 
 $email = $_POST['email'];
-$subject = $_POST['subject'];
+$subject = "[myWWW message] " . $_POST['subject'];
 $text = "";
+$returnMessage = "";
+$sendReturn=true;
 
 if ($email=="") {
     $email="Anonym";
+    $sendReturn=false;
 }
 
 $text .= "$email wrote:\n";
 $text .= $_POST['text'];
+
+
+
 $date = date("r");
 
 header('Content-Type: text/html; charset=utf-8');
-header( "refresh:5;url=contact.php?lang=$lang" );
-
-
+header( "refresh:3;url=contact.php?lang=$lang" );
 
 mail("arturwrona91@gmail.com", 
       $subject, 
       $text);
 
-printf("<p> %s", $date);
-printf("<p> Lang:  %s", $lang);
-printf("<p> E-mail: %s </p> <p> Subject: %s </p><p> Contents: <br> %s </p>", $email, $subject, $text);
-printf("<p> Message was <b>sent</b>.</p>");
-printf("<p>Poczekaj 5 sekund lub kliknij: <a href=contact.php?lang=$lang> Powrót to poprzedniej strony </a></p>");
+$returnMessage = <<<END_RETURN
+Dear <b>$email</b><br>
+<p>Thank you for send me a message</p>
+<p>I will reply You soon as possible</p>
+<br><br>
+Contents:<br>
+Date: $date
+E-mail: $email</p> 
+<p>Subject: $subject </p>
+<p>Contents: <br>$text</p>
+<p>Message was <b>sent</b></p>
+END_RETURN;
+
+if ($sendReturn) {
+mail($email, "Re: $subject", "$returnMessage");
+}
+
+printf("$returnMessage <p>Please wait or click: <a href=contact.php?lang=$lang> Get back to last page</a></p>");
 
 
 ?>
